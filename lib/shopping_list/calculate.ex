166dd02483e5d6a5aefd412de8f97bee2c9858b8
Alexandra Souza <alexandrabsouz@gmail.com>
2022-08-08 13:16:40 -0300

    refactor: sum values

--- lib/shopping_list/calculate.ex
+++ lib/shopping_list/calculate.ex
@@ -2,9 +2,12 @@ defmodule ShoppingList.Calculate do
 
   #@items [["maçã", 10, 200], ["pizza", 5, 40000], ["carne", 5, 4000]]
 
-  def sum_values(items) do
-    items
-    |> Enum.map(fn [_item, quantity, value] -> quantity * value end)
-    |> Enum.sum()
+  def sum_values(items) when is_list(items) do
+    total =
+      items
+      |> Enum.map(fn [_item, quantity, value] -> quantity * value end)
+      |> Enum.sum()
+
+    {:ok, total}
   end
 end
