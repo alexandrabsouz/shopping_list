8914dcf41de5558da08613eabafa0339138ef8b3
Alexandra Souza <alexandrabsouz@gmail.com>
2022-08-08 09:46:51 -0300

    chore: returns sum of list

--- lib/shopping_list/calculate.ex
+++ lib/shopping_list/calculate.ex
@@ -1,9 +1,10 @@
 defmodule ShoppingList.Calculate do
 
-  @items [["maçã", 10, 200], ["pizza", 5, 40000], ["carne", 5, 4000]]
+  #@items [["maçã", 10, 200], ["pizza", 5, 40000], ["carne", 5, 4000]]
 
-  def multiplicate_values do
-    @items
-    |> Enum.map(fn [item, quantity, value] -> [item, quantity * value] end)
+  def sum_values(items) do
+    items
+    |> Enum.map(fn [_item, quantity, value] -> quantity * value end)
+    |> Enum.sum()
   end
 end
