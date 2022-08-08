bdd2d3b1396ebcee04274cbd57f126904382dc98
Alexandra Souza <alexandrabsouz@gmail.com>
2022-08-08 13:19:44 -0300

    feat: fetch lists

--- lib/shopping_list.ex
+++ lib/shopping_list.ex
@@ -1,4 +1,6 @@
 defmodule ShoppingList do
+  alias ShoppingList.Calculate
+
   @moduledoc """
   Documentation for `ShoppingList`.
   """
@@ -12,7 +14,16 @@ defmodule ShoppingList do
       :world
 
   """
-  def hello do
-    
+
+  @emails ["alexandra@email.com", "pedro@email.com", "paulo@email.com"]
+  @items [["maçã", 10, 300], ["pizza", 5, 40000], ["carne", 5, 4000]]
+
+  def fetch_lists(items \\ @items, emails \\ @emails) do
+    with {:ok, total} <- Calculate.sum_values(items),
+         uniq_emails <- Enum.uniq(emails),
+         count_emails <- Enum.count(uniq_emails),
+         map_emails <- Enum.map(uniq_emails, fn email -> %{email: email, value: nil} end) do
+      Calculate.split_bill(total, count_emails, map_emails)
+    end
   end
 end
