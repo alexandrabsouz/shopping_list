229e31b3f9018c80e949ed0549eee4d74fbe5838
Alexandra Souza <alexandrabsouz@gmail.com>
2022-08-08 16:23:52 -0300

    test: fetch lists all params valids

--- test/shopping_list_test.exs
+++ test/shopping_list_test.exs
@@ -2,7 +2,26 @@ defmodule ShoppingListTest do
   use ExUnit.Case
   doctest ShoppingList
 
-  test "greets the world" do
-    assert ShoppingList.hello() == :world
+  alias ShoppingList
+
+  @emails ["alexandra@email.com", "pedro@email.com", "paulo@email.com"]
+  @duplicated_emails ["alexandra@email.com", "pedro@email.com", "paulo@email.com", "paulo@email.com"]
+
+  @items [["maçã", 10, 300], ["pizza", 5, 10], ["carne", 5, 4000]]
+  @items_inválid_quantity [["maçã", 0, 300], ["pizza", 5, 10], ["carne", 5, 4000]]
+  @items_inválid_value [["maçã", 4, 300], ["pizza", 5, 0], ["carne", 5, 4000]]
+
+  describe "fetch_lists/2" do
+    test "when all params is valid, returns split bill" do
+      response = ShoppingList.fetch_lists(@items, @emails)
+
+      expected_response = [
+        %{email: "alexandra@email.com", value: 7684},
+        %{email: "pedro@email.com", value: 7683},
+        %{email: "paulo@email.com", value: 7683}
+      ]
+
+      assert response == expected_response
+    end
   end
 end
