b316d7cfda14f28b4fc7f99138dc51b79f310f46
Alexandra Souza <alexandrabsouz@gmail.com>
2022-08-08 13:17:54 -0300

    feat: split bill values

--- lib/shopping_list/calculate.ex
+++ lib/shopping_list/calculate.ex
@@ -1,6 +1,5 @@
 defmodule ShoppingList.Calculate do
-
-  #@items [["maçã", 10, 200], ["pizza", 5, 40000], ["carne", 5, 4000]]
+  # @items [["maçã", 10, 200], ["pizza", 5, 40000], ["carne", 5, 4000]]
 
   def sum_values(items) when is_list(items) do
     total =
@@ -9,5 +8,27 @@ defmodule ShoppingList.Calculate do
       |> Enum.sum()
 
     {:ok, total}
+  end
+
+  def split_bill(total, count_emails, emails) do
+    value_per_email = div(total, count_emails)
+    rest = rem(total, count_emails)
+
+    emails
+      |> set_value_in_emails(value_per_email)
+      |> maybe_increase_rest(rest)
+  end
+
+  defp set_value_in_emails(emails, value_per_email) do
+    Enum.map(emails, fn email -> %{email | value: value_per_email} end)
+  end
+
+  defp maybe_increase_rest(emails, 0), do: emails
+
+  defp maybe_increase_rest(emails, rest) do
+    0..(rest - 1)
+    |> Enum.reduce(emails, fn pos, acc ->
+      List.update_at(acc, pos, fn email -> %{email | value: email.value + 1} end)
+    end)
   end
 end
